62c7c9e636fb788be009a7aee9994d2b4fab5b78
GREATBIGNATE <greatbignate@gmail.com>
2018-08-13 12:30:30 -0700

    Scaffold and initial inputs

---
+++ js/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+// Define initial variables and arrays
+
+
+
+
+// var alki = {
+//     name: 'Alki Beach',
+//     neighborhood: 'West Seattle',
+//     seagullCount: [35,75,59,12,36,477,15],
+//     render: function(){
+//         // Access the element in the DOMwhere we will put data
+//         var alkiBeachUlEl = document.getElementById('alkibeach');    
+//         console.log(alkiBeachUlEl);
+//         // For each value in the array - 
+//         for (var i=0; i < alki.seagullCount.length; i++) {
+//             console.log(this.seagullCount[i]);
+//             // Create the <li> element
+//             var liEl = document.createElement('li');            
+//             // give the <li> element content
+//             liEl.textContent = days[i]+ ': ' +this.seagullCount[i];
+//             // Append the <li> elemnt to the <ul>
+//             alkiBeachUlEl.appendChild(liEl);
+//         };
+        
+//     }
+// };
+
+// var allBeaches = [alki, goldenGardens, edmondsBeach];
+// for (var i=0; i < allBeaches.length; i++) {
+//     allBeaches[i].render();
+// }
