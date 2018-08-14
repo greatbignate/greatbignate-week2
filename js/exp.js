4022f48b3db1c71478de49049918827a14ee3fb8
GREATBIGNATE <greatbignate@gmail.com>
2018-08-13 18:13:42 -0700

    added some CSS and fonts

---
+++ js/exp.js
@@ -0,0 +1,20 @@
+// Create arrays using for loop
+// var genCookieCount = function(a,b,c){
+
+// for (var i=0 ; i < hours.length ; i++) {
+//     if (i === 0) {
+//         var custInHour = [genRandom(a,b)];
+//         var cookieInHour = [Math.ceil(custInHour[i]*c)];
+//         var totalCookie = cookieInHour[i];
+//     } else {
+//         custInHour.push(genRandom(a,b));
+//         cookieInHour.push(Math.ceil(custInHour[i]*c));
+//         totalCookie = totalCookie + cookieInHour[i];
+//     }
+//     //console.log('i is ' +i+ ', cookieInHour is ' +cookieInHour[i]+ ' totalCookie is ' +totalCookie); 
+           
+// }
+// return [cookieInHour, totalCookie];
+// console.log ('Cookie array: ' +cookieInHour);
+// console.log (' Total Cookies: ' +totalCookie);
+// }
