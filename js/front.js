1aef0f3f0e406e2ddc94d33b9c60583c3530b990
GREATBIGNATE <greatbignate@gmail.com>
2018-08-14 18:16:12 -0700

    Charts done as they are going to be , CSS asthetics way below par

--- js/front.js
+++ js/front.js
@@ -1,6 +1,6 @@
-var bodyX = 219;
-var bodyY = 298;
-var bodyW = 319;
+var bodyX = 500;
+var bodyY = 500;
+var bodyW = 500;
 var bodyH = bodyW*0.95;
 var headW = bodyW/2;
 var headH = headW*0.79;
@@ -8,7 +8,9 @@ var headY = bodyY - ((bodyH+headH)/2-headH*0.33);
 var pupilSize = (headH*0.1);
 var pupilMax = (headH*0.15)-pupilSize;
 
-draw = function() {
+document.body.style.backgroundImage = "url('img/Bubba-n-Teenie.JPG')"; 
+
+//draw = function() {
     //left eye
     fill(252, 252, 252);
     ellipse(bodyX-headW*0.25, headY, headW*0.30, headH*0.30);//eye
@@ -24,4 +26,6 @@ draw = function() {
     var pupilRX = (bodyX+headW*0.25)+(1.5*(mouseX*2-400)/400)*pupilMax;
     var pupilRY = headY+(1.5*(mouseY*2-400)/400)*pupilMax;
     ellipse(pupilRX, pupilRY, pupilSize, pupilSize);
-};
+//};
+
+//draw();
